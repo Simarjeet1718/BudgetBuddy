9c1dd9f66e73d175c336866ef410a433831e902d
Simarjeet <simarjeetsingh1805@hmail.com>
2025-07-11 23:46:16 +0530

    Changes made by adding .env

--- src/firebase.js
+++ src/firebase.js
@@ -2,21 +2,18 @@
 
 
 import { initializeApp } from "firebase/app";
-
-
-
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, doc, setDoc,getDoc } from "firebase/firestore";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9sI868DV-mvAivGpFPldu3yIzflkM6e4",
-  authDomain: "budgetbuddy-a66d2.firebaseapp.com",
-  projectId: "budgetbuddy-a66d2",
-  storageBucket: "budgetbuddy-a66d2.appspot.com",
-  messagingSenderId: "221762885801",
-  appId: "1:221762885801:web:f688e030178da1cc1b31b8",
-  measurementId: "G-8J95MF5BFQ"
+  apiKey: import.meta.env.FIREBASE_API_KEY,
+  authDomain: import.meta.env.FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:import.meta.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId:import.meta.env.FIREBASE_APP_ID,
+  measurementId: import.meta.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
